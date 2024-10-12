9ed21ed928030a295da58cce2699952bbe12572b
chenxing <mitrecx@163.com>
2024-10-12 16:54:23 +0800

    init

---
+++ src/main/java/com/example/boot01web01/controller/HelloController.java
@@ -0,0 +1,38 @@
+package com.example.boot01web01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+    @GetMapping("/user")
+    public String getUser() {
+        System.out.println("getUser方法执行了 ...");
+        System.out.println("123...啥事");
+        return "GET-张三";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String saveUser() {
+        return "POST-张三";
+    }
+
+
+    @RequestMapping(value = "/user", method = RequestMethod.PUT)
+    public String putUser() {
+        return "PUT-张三";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.DELETE)
+    public String deleteUser() {
+        return "DELETE-张三";
+    }
+
+}
